317554a6b5c1be7c597d422f9da3c5536546e281
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2020-10-29 09:23:48 -0700

    Updates

---
+++ precache-manifest.8fa3d4ea82f0a16aa5c0815ae352e228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9999a425e3f09f86e7d4aa713c643715",
+    "url": "/recipe-search-app/index.html"
+  },
+  {
+    "revision": "40ca2b54e4c5fd9aef73",
+    "url": "/recipe-search-app/static/css/main.4a177553.chunk.css"
+  },
+  {
+    "revision": "e48225b3560f04c4f3ec",
+    "url": "/recipe-search-app/static/js/2.657b9f55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-search-app/static/js/2.657b9f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ca2b54e4c5fd9aef73",
+    "url": "/recipe-search-app/static/js/main.98788ffd.chunk.js"
+  },
+  {
+    "revision": "16a3230845e38df9e10e",
+    "url": "/recipe-search-app/static/js/runtime-main.760847be.js"
+  }
+]);
